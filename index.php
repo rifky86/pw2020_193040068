ed61b03820bebd874212a1815f00db0e98e3d72d
rifky86 <58411821+rifky86@users.noreply.github.com>
2020-05-21 04:40:45 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>
+    body {
+      background: -webkit-linear-gradient(bottom, #2dbd6e, #a6f77b);
+      background-repeat: no-repeat;
+
+    }
+
+    .card {
+      background: #fbfbfb;
+      border-radius: 8px;
+      box-shadow: 1px 2px 8px rgba(0, 0, 0, 0.65);
+      height: 600px;
+      margin: 6rem auto 8.1rem auto;
+      width: 800px;
+      text-align: center;
+
+    }
+
+    .card h2 {
+      margin: 40px;
+    }
+  </style>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <title>index</title>
+</head>
+
+<body>
+  <div class="card">
+    <h2>KULIAH</h2>
+    <div class="row">
+      <div class="col">
+        <div class="btn-group">
+          <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            pertemuan10
+          </button>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="Kuliah/Pertemuan10/latihan1.php">Latihan1</a>
+            <a class="dropdown-item" href="Kuliah/Pertemuan10/latihan2.php">Latihan2</a>
+            <a class="dropdown-item" href="Kuliah/Pertemuan10/latihan3.php">Latihan3</a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="btn-group">
+          <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            pertemuan11
+          </button>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="Kuliah/Pertemuan11/index.php">P11</a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="btn-group">
+          <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            pertemuan12
+          </button>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="Kuliah/Pertemuan12/index.php">P12</a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="btn-group">
+          <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            pertemuan13
+          </button>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="Kuliah/Pertemuan13/index.php">P13</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <h2>PRAKTIKUM</h2>
+    <div class="row">
+      <div class="col">
+        <div class="btn-group">
+          <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Peraktikum5
+          </button>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="Praktikum/P5_PW_KAMIS0840_193040068/index.php">P5</a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="btn-group">
+          <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Peraktikum6
+          </button>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="Praktikum/P6_PW_KAMIS0840_193040068/index.php">P6</a>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="btn-group">
+          <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Peraktikum7
+          </button>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="Praktikum/P5_PW_KAMIS0840_193040068/index.php">P7</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <h2>TUBES</h2>
+    <div class="row">
+      <div class="col">
+        <div class="btn-group">
+          <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Tubes
+          </button>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="tubes/193040068/index.php">Tubes</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
+</html>
